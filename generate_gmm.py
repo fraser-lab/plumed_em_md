bbae51f9fd6b5087ec191ff9f2b93550c6d45be0
fraserlab <jfraser@localhost.localdomain>
2018-07-08 11:59:27 -0700

    re-uncommmented after debug

--- generate_gmm.py
+++ generate_gmm.py
@@ -12,30 +12,30 @@ args = parser.parse_args()
 n_proc = int(args.n_proc)
 input_map = args.input_map
 
-# os.system("{gmconvert} -imap {map} -oimap output-0.0.mrc -zth 0.0".format(map=input_map, gmconvert=gmconvert_location))
-# os.mkdir("ITER_0")
-# os.mkdir("ITER_1")
-# os.chdir("ITER_0")
-# os.system("{gmconvert} V2G -imap ../output-0.0.mrc -ogmm 0.gmm -ng 150 -zth 0.0".format(gmconvert=gmconvert_location))
-# os.chdir("../")
-#
-# os.chdir("ITER_1")
-# for i in range(0,150,1):
-#     os.mkdir("map_0_{i}".format(i=i))
-#     os.chdir("map_0_{i}".format(i=i))
-#     os.system("echo \"../../ITER_0/0.gmm {i}\" > GMM.list".format(i=i))
-#     os.chdir("../")
-#
-# def create_gmms(i):
-#     os.chdir("map_0_{i}".format(i=i))
-#     os.system("{gmconvert} -imap ../../{map} -gmml GMM.list -ng 100 -ogmm {i}_0.gmm -zth 0.0".format(map=input_map,i=i,gmconvert=gmconvert_location))
-#     os.chdir("../")
-#     return
-#
-# p = Pool(n_proc)
-# print(p.map(create_gmms,range(0,150,1)))
-#
-# os.system("cat map_*/*.gmm > 1.gmm")
+os.system("{gmconvert} -imap {map} -oimap output-0.0.mrc -zth 0.0".format(map=input_map, gmconvert=gmconvert_location))
+os.mkdir("ITER_0")
+os.mkdir("ITER_1")
+os.chdir("ITER_0")
+os.system("{gmconvert} V2G -imap ../output-0.0.mrc -ogmm 0.gmm -ng 150 -zth 0.0".format(gmconvert=gmconvert_location))
+os.chdir("../")
+
+os.chdir("ITER_1")
+for i in range(0,150,1):
+    os.mkdir("map_0_{i}".format(i=i))
+    os.chdir("map_0_{i}".format(i=i))
+    os.system("echo \"../../ITER_0/0.gmm {i}\" > GMM.list".format(i=i))
+    os.chdir("../")
+
+def create_gmms(i):
+    os.chdir("map_0_{i}".format(i=i))
+    os.system("{gmconvert} -imap ../../{map} -gmml GMM.list -ng 100 -ogmm {i}_0.gmm -zth 0.0".format(map=input_map,i=i,gmconvert=gmconvert_location))
+    os.chdir("../")
+    return
+
+p = Pool(n_proc)
+print(p.map(create_gmms,range(0,150,1)))
+
+os.system("cat map_*/*.gmm > 1.gmm")
 os.system("{gmconvert} VcmpG -igmm 1.gmm -imap ../{map} -zth 0.0 -omap iter_1.mrc > iter_1.log".format(map=input_map, gmconvert=sbgrid_gmconvert))
 
 from subprocess import check_output
