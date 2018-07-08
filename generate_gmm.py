c3c62b8aa0b98ad49ae8c389b439be14fd591fdb
fraserlab <jfraser@localhost.localdomain>
2018-07-08 11:58:51 -0700

    formatting for final CCgrid

--- generate_gmm.py
+++ generate_gmm.py
@@ -12,29 +12,39 @@ args = parser.parse_args()
 n_proc = int(args.n_proc)
 input_map = args.input_map
 
-os.system("{gmconvert} -imap {map} -oimap output-0.0.mrc -zth 0.0".format(map=input_map, gmconvert=gmconvert_location))
-os.mkdir("ITER_0")
-os.mkdir("ITER_1")
-os.chdir("ITER_0")
-os.system("{gmconvert} V2G -imap ../output-0.0.mrc -ogmm 0.gmm -ng 150 -zth 0.0".format(gmconvert=gmconvert_location))
-os.chdir("../")
+# os.system("{gmconvert} -imap {map} -oimap output-0.0.mrc -zth 0.0".format(map=input_map, gmconvert=gmconvert_location))
+# os.mkdir("ITER_0")
+# os.mkdir("ITER_1")
+# os.chdir("ITER_0")
+# os.system("{gmconvert} V2G -imap ../output-0.0.mrc -ogmm 0.gmm -ng 150 -zth 0.0".format(gmconvert=gmconvert_location))
+# os.chdir("../")
+#
+# os.chdir("ITER_1")
+# for i in range(0,150,1):
+#     os.mkdir("map_0_{i}".format(i=i))
+#     os.chdir("map_0_{i}".format(i=i))
+#     os.system("echo \"../../ITER_0/0.gmm {i}\" > GMM.list".format(i=i))
+#     os.chdir("../")
+#
+# def create_gmms(i):
+#     os.chdir("map_0_{i}".format(i=i))
+#     os.system("{gmconvert} -imap ../../{map} -gmml GMM.list -ng 100 -ogmm {i}_0.gmm -zth 0.0".format(map=input_map,i=i,gmconvert=gmconvert_location))
+#     os.chdir("../")
+#     return
+#
+# p = Pool(n_proc)
+# print(p.map(create_gmms,range(0,150,1)))
+#
+# os.system("cat map_*/*.gmm > 1.gmm")
+os.system("{gmconvert} VcmpG -igmm 1.gmm -imap ../{map} -zth 0.0 -omap iter_1.mrc > iter_1.log".format(map=input_map, gmconvert=sbgrid_gmconvert))
 
-os.chdir("ITER_1")
-for i in range(0,150,1):
-    os.mkdir("map_0_{i}".format(i=i))
-    os.chdir("map_0_{i}".format(i=i))
-    os.system("echo \"../../ITER_0/0.gmm {i}\" > GMM.list".format(i=i))
-    os.chdir("../")
+from subprocess import check_output
+out = check_output(["grep CCgrid iter_1.log"],shell=True)
 
-def create_gmms(i):
-    os.chdir("map_0_{i}".format(i=i))
-    os.system("{gmconvert} -imap ../../{map} -gmml GMM.list -ng 100 -ogmm {i}_0.gmm -zth 0.0".format(map=input_map,i=i,gmconvert=gmconvert_location))
-    os.chdir("../")
-    return
+print("""
 
-p = Pool(n_proc)
-print(p.map(create_gmms,range(0,150,1)))
+CORRELATION BETWEEN GMM and ORIGINAL MAP:
 
-os.system("cat map_*/*.gmm > 1.gmm")
-os.system("{gmconvert} VcmpG -igmm 1.gmm -imap {map} -zth 0.0 -omap iter_1.mrc > iter_1.log".format(map=input_map, gmconvert=sbgrid_gmconvert))
-os.system("grep CCgrid iter_1.log")
+%s
+
+""" %out)
