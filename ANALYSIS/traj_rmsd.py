4eba6c70ddaa3e8f158cedccbc6291a4e3ac6fd7
James Fraser <jfraser@fraserlab.com>
2018-07-30 11:57:44 -0700

    rmsd comments

--- ANALYSIS/traj_rmsd.py
+++ ANALYSIS/traj_rmsd.py
@@ -16,27 +16,33 @@ n = len(u.trajectory)
 # open output log file
 log=open(sys.argv[3], "w")
 
-# first and last frame
+# first and last element of the 1D vector created below
+# for everything include 0 as first
+# and
+# if N if the number of frames in the trajectory,
+# then k1 = N * ( N - 1 ) / 2
+# because last number will be excluded and python counts from 0
+
 k0 = int(sys.argv[4])
 k1 = int(sys.argv[5])
 
 # cycle on pairs and write on a separate file
 for k in range(k0, k1):
-    # map one-dimensional index to two-dimensional indexes 
+    # map one-dimensional index to two-dimensional indexes
     i = int(n - 2 - math.floor(math.sqrt(-8*k + 4*n*(n-1)-7)/2.0 - 0.5))
-    j = int(k + i + 1 - n*(n-1)/2 + (n-i)*((n-i)-1)/2) 
+    j = int(k + i + 1 - n*(n-1)/2 + (n-i)*((n-i)-1)/2)
     # go to frame i
     u.trajectory[i]
     # copy positions of all-heavy atoms
     A = allheavy.positions.copy()
     # go to frame j
     u.trajectory[j]
-    # copy positions of all-heavy atoms 
+    # copy positions of all-heavy atoms
     B = allheavy.positions.copy()
     # calculate RMSD with optimal alignment - put False to deactivate alignment
     rmsd = MDAnalysis.analysis.rms.rmsd(A, B, superposition=True)
     # print on file
-    log.write("%6d %6d %6.3f\n" % (i, j, rmsd)) 
+    log.write("%6d %6d %6.3f\n" % (i, j, rmsd))
 
 # close file
 log.close()
