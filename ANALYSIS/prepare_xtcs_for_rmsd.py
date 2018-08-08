494e687a4a3d210b11c3b3f6c7041b9b396c5fb7
James Fraser <jfraser@fraserlab.com>
2018-08-08 09:56:43 -0700

    updated parsing for preparation

--- ANALYSIS/prepare_xtcs_for_rmsd.py
+++ ANALYSIS/prepare_xtcs_for_rmsd.py
@@ -1,4 +1,11 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--prefix", help="name for xtc",required=True)
+args = parser.parse_args()
+prefix = args.prefix
+
+
 import os
 
 os.system("gmx_mpi trjcat -f traj_comp?.xtc -o traj_all.xtc -cat")
-os.system("gmx_mpi trjconv -f traj_all.xtc -o AC_SYMM_reconstruct_all.xtc -pbc whole -s topol0.tpr")
+os.system("gmx_mpi trjconv -f traj_all.xtc -o {prefix}.xtc -pbc whole -s topol0.tpr".format(prefix=prefix))
