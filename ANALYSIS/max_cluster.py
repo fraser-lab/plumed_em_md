6915bb1bbf3b9fa264b0b71e30f7cdee106e2adb
James Fraser <jfraser@fraserlab.com>
2018-08-08 13:17:38 -0700

    check frame number in clustering

--- ANALYSIS/max_cluster.py
+++ ANALYSIS/max_cluster.py
@@ -1,4 +1,16 @@
+from subprocess import check_output
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--xtc", help="trajectory xtc file",required=True)
+args = parser.parse_args()
+xtc = args.xtc
+
+
 #GET NUMBER OF frames
+out = check_output(["gmx_mpi check -f {xtc}".format(xtc=xtc)],shell=True)
+
+print(out)
 #divide into number of processors
 #farm out RMSD calculations
 #concatenate RMSD logs
