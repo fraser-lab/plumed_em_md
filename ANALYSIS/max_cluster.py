0f0d5283ee70034bb286e8e576a8be64834015ee
James Fraser <jfraser@fraserlab.com>
2018-08-08 13:39:32 -0700

    assigning jobs

--- ANALYSIS/max_cluster.py
+++ ANALYSIS/max_cluster.py
@@ -4,16 +4,26 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--xtc", help="trajectory xtc file",required=True)
+parser.add_argument("--nproc", help="number of cores",type=intrequired=True)
 args = parser.parse_args()
 xtc = args.xtc
-
+nproc = args.nproc
 
 #GET NUMBER OF frames
 os.system("gmx_mpi check -f {xtc} 2> temp.txt".format(xtc=xtc))
 out = check_output(["grep Box temp.txt"],shell=True)
 frames = int(out.split()[-1])
-print(frames)
+
 #divide into number of processors
+#max number of calculations k1 = N * ( N - 1 ) / 2
+max_calc = int((frames*(frames-1))/2)
+#number of calcs per job
+calcs_per_job = max_calc/nproc
+jobs = list(range(0,max_calc,calcs_per_job))
+
+for i,calculation in enumerate(jobs):
+    print(i,calculations,jobs[i+1])
+
 #farm out RMSD calculations
 #concatenate RMSD logs
 #cluster
